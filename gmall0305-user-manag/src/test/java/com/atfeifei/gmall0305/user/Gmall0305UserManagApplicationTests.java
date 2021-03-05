0eec2528d009500d4d909336f3d541427b5dabfe
tongchaofei <tcf00159@163.com>
2021-03-05 18:07:15 +0800

    加入用户管理模块

---
+++ gmall0305-user-manag/src/test/java/com/atfeifei/gmall0305/user/Gmall0305UserManagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atfeifei.gmall0305.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall0305UserManagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c1e7de0f35423afbc06b90f3cebda862185ce69a
tongchaofei <tcf00159@163.com>
2021-03-05 18:11:05 +0800

    加入用户管理模块

---
+++ gmall0305-user-manag/src/main/java/com/atfeifei/gmall0305/user/Gmall0305UserManagApplication.java
@@ -0,0 +1,13 @@
+package com.atfeifei.gmall0305.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0305UserManagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0305UserManagApplication.class, args);
+    }
+
+}
